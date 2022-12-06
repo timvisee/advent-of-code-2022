0109ddb04fdda1351ed7f73f7fba37ab158a88ba
timvisee <tim@visee.me>
2022-12-06 11:32:29 +0100

    Optimize range in day06

--- day06a/src/main.rs
+++ day06a/src/main.rs
@@ -3,7 +3,7 @@ pub fn main() {
         "{}",
         include_bytes!("../input.txt")
             .windows(4)
-            .position(|b| !(0..4).any(|i| (i + 1..4).any(|j| b[i] == b[j])))
+            .position(|b| !(0..3).any(|i| (i + 1..4).any(|j| b[i] == b[j])))
             .unwrap()
             + 4,
     );
--- day06b/src/main.rs
+++ day06b/src/main.rs
@@ -3,7 +3,7 @@ pub fn main() {
         "{}",
         include_bytes!("../input.txt")
             .windows(14)
-            .position(|b| !(0..14).any(|i| (i + 1..14).any(|j| b[i] == b[j])))
+            .position(|b| !(0..13).any(|i| (i + 1..14).any(|j| b[i] == b[j])))
             .unwrap()
             + 14,
     );
