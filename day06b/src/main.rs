97f44a91ace6fff947377d59f45a91054bea0e14
timvisee <tim@visee.me>
2022-12-06 15:59:45 +0100

    Fix off-by-one error in day06b

--- day06b/src/main.rs
+++ day06b/src/main.rs
@@ -4,10 +4,10 @@ pub fn main() {
     let mut w = 0;
     'main: loop {
         let mut seen = 0u32;
-        for i in (1..14).rev() {
+        for i in (1..=14).rev() {
             let mask = 1 << d[w + i] - b'a';
             if seen & mask == mask {
-                w += i + 1;
+                w += i;
                 continue 'main;
             }
             seen |= mask;
@@ -15,5 +15,5 @@ pub fn main() {
         break;
     }
 
-    println!("{}", w + 14);
+    println!("{}", w + 15);
 }
