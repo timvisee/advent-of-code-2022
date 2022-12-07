1f684f4fee1d005cd545fdce0fb584764bccb7cd
timvisee <tim@visee.me>
2022-12-07 17:51:23 +0100

    Use binary rather than strings to optimize day07

--- day07a/src/main.rs
+++ day07a/src/main.rs
@@ -1,20 +1,20 @@
 use std::iter::Peekable;
 
 pub fn main() {
-    let (mut lines, mut sum) = (include_str!("../input.txt").lines().peekable(), 0);
-    sh(&mut lines, &mut sum);
-    println!("{}", sum);
+    let (d, mut s) = (include_bytes!("../input.txt"), 0);
+    sh(&mut d.split(|b| b == &b'\n').peekable(), &mut s);
+    println!("{}", s);
 }
 
-fn sh(lines: &mut Peekable<impl Iterator<Item = &'static str>>, sum: &mut usize) -> usize {
+fn sh(lines: &mut Peekable<impl Iterator<Item = &'static [u8]>>, sum: &mut u64) -> u64 {
     let mut size = 0;
     while let Some(i) = lines.next() {
         match i {
-            "$ cd .." => break,
-            _ if i.starts_with("$ l") => {
-                size = std::iter::from_fn(|| lines.next_if(|i| !i.starts_with('$')))
-                    .filter(|i| !i.starts_with('d'))
-                    .map(|i| i.split(' ').next().unwrap().parse::<usize>().unwrap())
+            b"$ cd .." => break,
+            _ if &i[0..3] == b"$ l" => {
+                size = std::iter::from_fn(|| lines.next_if(|i| i[0] != b'$'))
+                    .filter(|i| i[0] != b'd')
+                    .filter_map(|i| atoi::atoi::<u64>(i.split(|b| b == &b' ').next().unwrap()))
                     .sum()
             }
             _ => size += sh(lines, sum),
--- day07b/src/main.rs
+++ day07b/src/main.rs
@@ -1,31 +1,33 @@
 use std::iter::Peekable;
 
-struct Dir(Vec<Dir>, usize);
+struct Dir(Vec<Dir>, u64);
 
 pub fn main() {
-    let root = sh(&mut include_str!("../input.txt").lines().peekable());
+    let root = sh(&mut include_bytes!("../input.txt")
+        .split(|b| b == &b'\n')
+        .peekable());
     println!("{}", search(&root, root.1 - 40_000_000).unwrap());
 }
 
-fn sh(lines: &mut Peekable<impl Iterator<Item = &'static str>>) -> Dir {
+fn sh(lines: &mut Peekable<impl Iterator<Item = &'static [u8]>>) -> Dir {
     let (mut dirs, mut size) = (vec![], 0);
     while let Some(i) = lines.next() {
         match i {
-            "$ cd .." => break,
-            _ if i.starts_with("$ l") => {
-                size = std::iter::from_fn(|| lines.next_if(|i| !i.starts_with('$')))
-                    .filter(|i| !i.starts_with('d'))
-                    .map(|i| i.split(' ').next().unwrap().parse::<usize>().unwrap())
+            b"$ cd .." => break,
+            _ if &i[0..3] == b"$ l" => {
+                size = std::iter::from_fn(|| lines.next_if(|i| i[0] != b'$'))
+                    .filter(|i| i[0] != b'd')
+                    .filter_map(|i| atoi::atoi::<u64>(i.split(|b| b == &b' ').next().unwrap()))
                     .sum()
             }
             _ => dirs.push(sh(lines)),
         }
     }
-    size += dirs.iter().map(|d| d.1).sum::<usize>();
+    size += dirs.iter().map(|d| d.1).sum::<u64>();
     Dir(dirs, size)
 }
 
-fn search(d: &Dir, min: usize) -> Option<usize> {
+fn search(d: &Dir, min: u64) -> Option<u64> {
     d.0.iter()
         .filter(|d| d.1 >= min)
         .flat_map(|d| [Some(d.1), search(d, min)])
