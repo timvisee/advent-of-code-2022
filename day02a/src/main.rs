2f37578b8be3d6ba7dbe54f0d180caf56f761df1
timvisee <tim@visee.me>
2022-12-12 22:28:29 +0100

    Remove redundant brackets

--- day02a/src/main.rs
+++ day02a/src/main.rs
@@ -4,7 +4,7 @@ pub fn main() {
         include_bytes!("../input.txt")
             .split(|b| *b == b'\n')
             .map(|l| ((l[0] - b'A') as i16, (l[2] - b'X') as i16,))
-            .map(|(a, b)| 1 + b + 3 * ((1 + b - a).rem_euclid(3)))
+            .map(|(a, b)| 1 + b + 3 * (1 + b - a).rem_euclid(3))
             .sum::<i16>(),
     );
 }
