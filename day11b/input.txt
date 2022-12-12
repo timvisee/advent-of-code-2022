6f78fac81ce86736d3f797a58458e6b74dc71b3b
timvisee <tim@visee.me>
2022-12-12 13:55:54 +0100

    Add day11b

---
+++ day11b/input.txt
@@ -0,0 +1,55 @@
+Monkey 0:
+  Starting items: 66, 71, 94
+  Operation: new = old * 5
+  Test: divisible by 3
+    If true: throw to monkey 7
+    If false: throw to monkey 4
+
+Monkey 1:
+  Starting items: 70
+  Operation: new = old + 6
+  Test: divisible by 17
+    If true: throw to monkey 3
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 62, 68, 56, 65, 94, 78
+  Operation: new = old + 5
+  Test: divisible by 2
+    If true: throw to monkey 3
+    If false: throw to monkey 1
+
+Monkey 3:
+  Starting items: 89, 94, 94, 67
+  Operation: new = old + 2
+  Test: divisible by 19
+    If true: throw to monkey 7
+    If false: throw to monkey 0
+
+Monkey 4:
+  Starting items: 71, 61, 73, 65, 98, 98, 63
+  Operation: new = old * 7
+  Test: divisible by 11
+    If true: throw to monkey 5
+    If false: throw to monkey 6
+
+Monkey 5:
+  Starting items: 55, 62, 68, 61, 60
+  Operation: new = old + 7
+  Test: divisible by 5
+    If true: throw to monkey 2
+    If false: throw to monkey 1
+
+Monkey 6:
+  Starting items: 93, 91, 69, 64, 72, 89, 50, 71
+  Operation: new = old + 1
+  Test: divisible by 13
+    If true: throw to monkey 5
+    If false: throw to monkey 2
+
+Monkey 7:
+  Starting items: 76, 50
+  Operation: new = old * old
+  Test: divisible by 7
+    If true: throw to monkey 4
+    If false: throw to monkey 6
---
+++ day11b/src/main.rs
@@ -0,0 +1,60 @@
+#![feature(box_syntax)]
+
+use itertools::Itertools;
+
+struct Monkey {
+    bag: Vec<usize>,
+    op: Box<dyn Fn(usize) -> usize>,
+    div: usize,
+    yay: usize,
+    nay: usize,
+    ins: usize,
+}
+
+pub fn main() {
+    let mut m: Vec<_> = include_str!("../input.txt")
+        .split("\n\n")
+        .map(|m| {
+            let l: Vec<_> = m.lines().map(|l| l.split(": ").last().unwrap()).collect();
+            Monkey {
+                bag: l[1].split(", ").map(|n| n.parse().unwrap()).collect(),
+                op: {
+                    let op: Vec<_> = l[2].rsplit_once("= ").unwrap().1.split(' ').collect();
+                    match op[2] {
+                        "old" => box |old| old * old,
+                        b => match (op[1], b.parse::<usize>().unwrap()) {
+                            ("+", n) => box move |old| old + n,
+                            ("*", n) => box move |old| old * n,
+                            _ => unreachable!(),
+                        },
+                    }
+                },
+                div: l[3].rsplit_once(' ').unwrap().1.parse().unwrap(),
+                yay: l[4].rsplit_once(' ').unwrap().1.parse().unwrap(),
+                nay: l[5].rsplit_once(' ').unwrap().1.parse().unwrap(),
+                ins: 0,
+            }
+        })
+        .collect();
+    let (mo, mut bags): (usize, _) = (m.iter().map(|m| m.div).product(), vec![vec![]; m.len()]);
+
+    (0..10_000).for_each(|_| {
+        m.iter_mut().enumerate().for_each(|(i, m)| {
+            m.bag.append(&mut bags[i]);
+            m.bag.drain(0..).for_each(|mut n| {
+                n = (m.op)(n) % mo;
+                bags[if n % m.div == 0 { m.yay } else { m.nay }].push(n);
+                m.ins += 1;
+            });
+        });
+    });
+
+    println!(
+        "{}",
+        m.iter()
+            .map(|m| m.ins)
+            .sorted_unstable_by(|a, b| b.cmp(a))
+            .take(2)
+            .product::<usize>()
+    );
+}
